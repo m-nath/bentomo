b6ac53adb558d342bcc448c5bac511b26da10968
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-20 18:31:08 +0900

    add form!

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -12,4 +12,20 @@ class PlansController < ApplicationController
   def show
     @plan = policy_scope(Plan).find(params[:id])
   end
+
+  def new
+    @plan = Plan.new
+    authorize @plan
+  end
+
+  def create
+    @plan = Plan.new(plans_params)
+    @plan.user = current_user
+    authorize @plan
+    if @plan.save
+      redirect_to plan_path(@plan)
+    else
+      render :new
+    end
+  end
 end
