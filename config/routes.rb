b125b4582243f90221c44cc1ef95f66604354537
Christee Song <christee.song@gmail.com>
2019-08-20 12:22:03 +0900

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'uikit', to: 'pages#uikit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :kitchens, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :plans, only: [:new, :create]
+    resources :dishes, only: [:new, :create]
+  end
+
+  resources :plans, only: [:index, :show, :edit, :update, :destroy] do
+    resources :dish_plans, only: [:create]
+    resources :orders, only: [:create]
+  end
+
+  resources :orders, only: [:index, :show, :edit, :update] do
+    resources :payment, only: [:new, :create]
+  end
+
+  get 'my_account', to: 'users#edit'
+  resources :users, only: [:update]
+  resources :dish_plans, only: [:destroy]
+  resources :locations, except:[:index, :show]
+
 end
