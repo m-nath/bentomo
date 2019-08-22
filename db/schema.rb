dda4a0586bd1b3d6791779e636c1746927fd189f
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-22 14:20:52 +0900

    add price to plan table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_042029) do
+ActiveRecord::Schema.define(version: 2019_08_22_051814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
