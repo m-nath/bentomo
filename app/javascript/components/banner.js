eb202a9db9d2617836f11d71c394a49d4f1837f9
Christee Song <christee.song@gmail.com>
2019-08-23 12:00:03 +0900

    change banner text color

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Adventures start from good cocktails"],
+    strings: ["Tokyo tastes home again. "],
     typeSpeed: 50,
     loop: true
   });
