79ef136d4926e26cc31de0dff8d37317c31351e6
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-23 14:21:56 +0900

    fixed-order-db

--- db/migrate/20190820084854_add_request_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddRequestToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_column :orders, :request, :text
-  end
-end
--- db/migrate/20190821021316_add_date_time_to_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddDateTimeToOrders < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :orders, :date
-    add_column :orders, :date, :datetime
-  end
-end
