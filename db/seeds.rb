1940496cbcd64c059a6cf49a0adfbb432402c165
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-28 14:59:35 +0900

    change pic in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -223,7 +223,7 @@ Dish.create!(
 Dish.create!(
   name: 'Chesse pasta',
   kitchen: kitchen_2,
-  remote_photo_url: "https://res.cloudinary.com/dxouryvao/image/upload/v1566802095/ynuqrlvz5jzyjsxt8ks6.jpg"
+  remote_photo_url: "https://res.cloudinary.com/dxouryvao/image/upload/v1566971057/cux6tsq6kzvufcngn1ww.jpg"
 )
 
 kitchen_2.dishes.take(5).each do |dish|
@@ -640,11 +640,11 @@ end
 
 User.all.each do |user|
   # 2.times do
-    Location.create!(
-      label: ["Home", "Work"].sample,
-      address: ["1-3-21, Meguro, Meguro-ku, Tokyo", "3-13-6, Meguro, Meguro-ku, Tokyo", "1-21-20, Higashiyama, Meguro-ku, Tokyo", "‎2 Shimomeguro, Meguro-ku, Tokyo", "1-24-9, Meguro, Meguro-ku, Tokyo", "2 Chome-20-8 Shimomeguro, Meguro City, Tokyo", "2-19-15 Kamimeguro, Meguro-ku, Tokyo", "1-8-1 Shimomeguro, Meguro-ku, Tokyo", "4-1-1 Shimomeguro, Meguro-ku, Tokyo", "2-4-36 Meguro, Meguro-ku, Tokyo"].sample,
-      user: user  #real office addres e.g. Google, Amazon, Rakuten, Impacthub
-    )
+  Location.create!(
+    label: ["Home", "Work"].sample,
+    address: ["1-3-21, Meguro, Meguro-ku, Tokyo", "3-13-6, Meguro, Meguro-ku, Tokyo", "1-21-20, Higashiyama, Meguro-ku, Tokyo", "‎2 Shimomeguro, Meguro-ku, Tokyo", "1-24-9, Meguro, Meguro-ku, Tokyo", "2 Chome-20-8 Shimomeguro, Meguro City, Tokyo", "2-19-15 Kamimeguro, Meguro-ku, Tokyo", "1-8-1 Shimomeguro, Meguro-ku, Tokyo", "4-1-1 Shimomeguro, Meguro-ku, Tokyo", "2-4-36 Meguro, Meguro-ku, Tokyo"].sample,
+    user: user  #real office addres e.g. Google, Amazon, Rakuten, Impacthub
+  )
   # end
 end
 
@@ -653,16 +653,16 @@ christee.locations.each do |location|
 end
 
 Location.create!(
-      label: "Work",
-      address: "2-11-3, Meguro, Meguro-ku, Tokyo",
-      user: christee  #real office addres e.g. Google, Amazon, Rakuten, Impacthub
-    )
+  label: "Work",
+  address: "2-11-3, Meguro, Meguro-ku, Tokyo",
+  user: christee  #real office addres e.g. Google, Amazon, Rakuten, Impacthub
+)
 
 Location.create!(
-      label: "Home",
-      address: "5-1-1 Meijijingumae, Shibuya-ku, Tokyo",
-      user: christee  #real office addres e.g. Google, Amazon, Rakuten, Impacthub
-    )
+  label: "Home",
+  address: "5-1-1 Meijijingumae, Shibuya-ku, Tokyo",
+  user: christee  #real office addres e.g. Google, Amazon, Rakuten, Impacthub
+)
 
 puts 'christee location made'
 
