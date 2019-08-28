b50fdcd2c9ab79207a9262b33660c50bf5e6d5a5
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-28 11:46:47 +0900

    make homepage searchable within HP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,27 @@ class PagesController < ApplicationController
   def home
     @kitchens = policy_scope(Kitchen).sample(SAMPLE_NUM)
     @plans = policy_scope(Plan).sample(SAMPLE_NUM)
+
+    if params[:search]
+      query = params[:search][:query]
+      @tags = params[:search][:tags].reject(&:blank?) if params[:search][:tags].present?
+
+      if query.present? && @tags != nil
+        @kitchens = policy_scope(Kitchen).global_search(query).tagged_with(@tags)
+        @plans = policy_scope(Plan).global_search(query).tagged_with(@tags)
+      elsif query.present?
+        @kitchens = policy_scope(Kitchen).global_search(query)
+        @plans = policy_scope(Plan).global_search(query)
+      elsif @tags.any?
+        @kitchens = policy_scope(Kitchen).tagged_with(@tags)
+        @plans = policy_scope(Plan).tagged_with(params[:tag])
+      end
+    elsif params[:tag].present?
+      @kitchens = policy_scope(Kitchen).tagged_with(params[:tag])
+    else
+      @kitchens = policy_scope(Kitchen)
+      @plans = policy_scope(Plan)
+    end
   end
 
   def uikit
