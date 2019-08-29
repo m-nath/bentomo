f9c04ad8f353c0844de59f97f49b39b322bc0c2a
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-29 16:49:48 +0900

    addiing address inprog

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,32 @@
+class LocationsController < ApplicationController
+  before_action :set_user
+
+  def create
+    @location = Location.new(location_params)
+    @user = current_user
+    @location = @user.locations
+    authorize @location
+    if @location.save
+      redirect_to root_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def location_params
+    params.require(:location).permit(:label, :address)
+  end
+end
+end
