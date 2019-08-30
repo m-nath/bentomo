161d53c50e9456669bdff5d35978decaf55c5d14
Huishu Jia <huishu_jia@hotmail.com>
2019-08-30 11:59:35 +0900

    deleted

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_001623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
-    t.text "ingr"
     t.integer "calories"
     t.integer "fat"
     t.integer "carbs"
